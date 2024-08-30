bfe1708541e6741cbdf5b7fa26eb0690f47a71e6
Maximus Azuokwu <maximusazuokwu@gmail.com>
2024-08-30 10:38:50 +0100

    Complete Security Update

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -29,7 +32,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("Flask_Key")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +59,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -278,8 +281,8 @@ def contact():
 # DON'T put your email and password here directly! The code will be visible when you upload to Github.
 # Use environment variables instead (Day 35)
 
-# MAIL_ADDRESS = os.environ.get("EMAIL_KEY")
-# MAIL_APP_PW = os.environ.get("PASSWORD_KEY")
+# MAIL_ADDRESS = os.getenv("EMAIL")
+# MAIL_APP_PW = os.getenv("PASSWORD_KEY")
 
 # @app.route("/contact", methods=["GET", "POST"])
 # def contact():
